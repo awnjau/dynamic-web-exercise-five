90fe8001cb6c2b23786107cd345789eec6440afe
awnjau <98540454+awnjau@users.noreply.github.com>
2023-11-07 08:32:42 -0500

    commit using desktop

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const indexRoute = require('./routes/index');
+//const singlePostRoute = require('./routes/singlePost');
+//const createPostRoute = require('./routes/createPost');
+
+app.use("/", indexRoute);
+
+  
+  app.listen(port, () => {
+    console.log(`Exercise five on port ${port}`)
+  })
