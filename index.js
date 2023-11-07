7d0be6b03516ce14c48fa1d218f0e724c378da51
awnjau <98540454+awnjau@users.noreply.github.com>
2023-11-07 09:31:27 -0500

    updated

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const firebase = require("firebase/app")
+
+//port
 const port = 4000;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0__x3SaPgbhol3uwbxst2jP49pilEYpg",
+    authDomain: "dynamic-web-exercise-fiv-6ad5c.firebaseapp.com",
+    projectId: "dynamic-web-exercise-fiv-6ad5c",
+    storageBucket: "dynamic-web-exercise-fiv-6ad5c.appspot.com",
+    messagingSenderId: "630776729293",
+    appId: "1:630776729293:web:cc144c5877b9c339fba384"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
 
 const indexRoute = require('./routes/index');
 //const singlePostRoute = require('./routes/singlePost');
